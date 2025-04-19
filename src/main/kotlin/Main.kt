306d3eb3bb76e133d86bc27b6d7e3c3908c702ab
dmytrolavrov <dimalavrov2004.06.16@gmail.com>
2025-04-19 09:58:43 +0300

    Implement arithmetic and logical expressions calculator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,65 @@
-package org.example
+fun expression7() {
+    val a = 7
+    val b = 120
+    val c = 5
+    val d = 17
+    
+    val result = (a + b + c - d) * 2 / 3 % 5 + c
+    println("7) (a + b + c - d) * 2 / 3 % 5 + c при a = $a, b = $b, c = $c, d = $d")
+    println("Результат: $result")
+}
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun expression8() {
+    val x = 30
+    val y = 40
+    val z = 50
+    val d = 18
+    
+    val result = (x < y) && (z < x) && (y > z) || (y > d)
+    println("\n8) (x < y) && (z < x) && (y > z) || (y > d) при x = $x, y = $y, z = $z, d = $d")
+    println("Результат: $result")
+}
+
+fun expression10() {
+    val a = 5
+    val b = 3
+    val c = 2
+    val d = 4
+    
+    val result = ((a - b) * c) * d * 2 + a % d - (c * d)
+    println("\n10) ((a - b) * c) * d * 2 + a % d - (c * d) при a = $a, b = $b, c = $c, d = $d")
+    println("Результат: $result")
+}
+
+fun expression16() {
+    val a = 9
+    val b = 2
+    val c = 70
+    val d = 7
+    
+    val result = ((a * c) / b) / d + 10 * a % b - (a / d)
+    println("\n16) ((a * c) / b) / d + 10a % b - (a / d) при a = $a, b = $b, c = $c, d = $d")
+    println("Результат: $result")
+}
+
+fun expression17() {
+    val a = 7
+    val b = 120
+    val c = 15
+    val d = 17
+    
+    val result = (19 * a + 3 * b - 2 * c * d) * d / 3 % 5 + c
+    println("\n17) (19a + 3b - 2c * d) * d / 3 % 5 + c при a = $a, b = $b, c = $c, d = $d")
+    println("Результат: $result")
+}
+
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    println("Обчислення виразів:")
+    println("-------------------")
+    
+    expression7()
+    expression8()
+    expression10()
+    expression16()
+    expression17()
 }
